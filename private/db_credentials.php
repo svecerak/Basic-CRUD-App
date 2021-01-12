b6aa01a2eaf52e2324322e951c6fc5aff898c107
Sasa <svecerak@uwo.ca>
2021-01-11 23:40:15 -0500

    provision remote sql server

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -2,16 +2,18 @@
 
 /**
  * Configuration for database connection
+ * 
+ * For the ease of deploying to heroku, I won't add this file to .gitignore
  */
 
  // Local connection
-define('DB_SERVER', "localhost");
-define('DB_USERNAME', "root");
-define('DB_PASS', "");
-define('DB_NAME', "demo");
+// define('DB_SERVER', "localhost");
+// define('DB_USERNAME', "root");
+// define('DB_PASS', "");
+// define('DB_NAME', "demo");
 
 // Remote connection
-// define('DB_SERVER', "");
-// define('DB_USERNAME', "");
-// define('DB_PASS', "");
-// define('DB_NAME', "");
+define('DB_SERVER', "db4free.net");
+define('DB_USERNAME', "vecerasa");
+define('DB_PASS', "mydemopw34");
+define('DB_NAME', "sasaphpcrud");
